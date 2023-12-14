289d4fdca1056736c85fe8bce1a31fc77bf9a3ea
ZzibaParvaneeh <138671129+ZzibaParvaneeh@users.noreply.github.com>
2023-12-14 20:16:09 +0700

    Create reu.js

---
+++ reu.js
@@ -0,0 +1,25 @@
+function Football() {
+  const shoot = () => {
+    alert("Great Shot!");
+  }
+
+  return (
+    <button onClick={shoot}>Take the shot!</button>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Football />);
+
+            function Football() {
+  const shoot = (a) => {
+    alert(a);
+  }
+
+  return (
+    <button onClick={() => shoot("Goal!")}>Take the shot!</button>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Football />);
